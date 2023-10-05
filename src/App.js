7a6f68e089e4aa0d8c1a421f045b88120602fa10
skyeler yearsley <s2krighty@gmail.com>
2023-10-05 00:47:16 -0500

    Usage of Props to make components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { useState } from 'react';
-
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 
 function App() {
 
 
-   const[entries, setEntries] = useState([{weight:175, date:'11-23-2023'}])
+   const[entries, setEntries] = useState([{weight:175, date:'11-23-2023'},{weight:165, date:'11-24-2023'}])
 
   return (
     <div >
-     <table>
-      <thead>
-        <tr>
-          <th> Entry Number</th>
-          <th> Wieght</th>
-          <th> Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td> 1</td>
-          <td> 175</td>
-          <td> 11-23-2023</td>
-        </tr>
-      </tbody>
-     </table>
+   <DisplayEntries parentEntries= {entries} />
     </div>
   );
 }
